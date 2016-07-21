5733da4173a73358b989214d63a6d7aed39adb58
Jason Thomas <jthomas@bvs.com>
2016-07-21 13:47:30 -0600

    Reduce resolution when CPU overuse is detected. Add H264 to 1.5 MBPS bandwidth limit

--- modules/xmpp/JingleSessionPC.js
+++ modules/xmpp/JingleSessionPC.js
@@ -348,6 +348,9 @@ JingleSessionPC.prototype.setOffer = function (jingleOfferIq, success, failure) 
 	this.remoteSDP.raw = this.remoteSDP.raw.replace('a=rtpmap:100 VP8/90000', 
 		"a=rtpmap:100 VP8/90000\r\na=fmtp:100 x-google-max-bitrate=" + 
 		parseInt(this.room.options.bandwidthLimit));
+	this.remoteSDP.raw = this.remoteSDP.raw.replace('a=rtpmap:107 H264/90000', 
+		"a=rtpmap:107 H264/90000\r\na=fmtp:107 x-google-max-bitrate=" + 
+		parseInt(this.room.options.bandwidthLimit));
     }
 
     var remotedesc
